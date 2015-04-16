b21e17a6898c8607c1ab13687f68a00b0c297bfa
philou <philippe.bourgau@gmail.com>
2015-04-16 06:44:39 +0200

    Automaticaly open pom file at intellij startup

--- hack.sh
+++ hack.sh
@@ -2,4 +2,4 @@
 
 ssh-keygen -f "~/.ssh/known_hosts" -R [localhost]:2222
 vagrant up
-ssh -X -p 2222 vagrant@localhost .idea/bin/idea.sh
+ssh -X -p 2222 vagrant@localhost .idea/bin/idea.sh /vagrant/pom.xml
