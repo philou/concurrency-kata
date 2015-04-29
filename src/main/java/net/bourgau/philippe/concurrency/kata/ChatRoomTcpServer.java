f9ce209c116280fa5de326074081e2d7e60b4cf8
philou <philippe.bourgau@gmail.com>
2015-04-29 05:42:24 +0000

    Start server socket on the server
    - tests are still failing, but with "address already in use"
    - add the server socket creation on the server

--- src/main/java/net/bourgau/philippe/concurrency/kata/ChatRoomTcpServer.java
+++ src/main/java/net/bourgau/philippe/concurrency/kata/ChatRoomTcpServer.java
@@ -1,8 +1,17 @@
 package net.bourgau.philippe.concurrency.kata;
 
+import java.net.ServerSocket;
+
 public class ChatRoomTcpServer implements ChatRoom {
+
+    private final ServerSocket serverSocket;
+
+    public ChatRoomTcpServer(int port) throws Exception {
+        serverSocket = new ServerSocket(port);
+    }
+
     @Override
-    public void enter(Client client) {
+    public void enter(Client client) throws Exception {
 
     }
 
