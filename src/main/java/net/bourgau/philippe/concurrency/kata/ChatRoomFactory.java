7e25c63a7d80ac60d1dea40bd241289bfee1193a
philou <philippe.bourgau@gmail.com>
2015-07-06 20:11:22 +0000

    Trying using a bounded thread pool
    Failure to create an os thread are gone but the benchmarks don't finish
    - use a bounded thread pool in the ChatRoomFactory

--- src/main/java/net/bourgau/philippe/concurrency/kata/ChatRoomFactory.java
+++ src/main/java/net/bourgau/philippe/concurrency/kata/ChatRoomFactory.java
@@ -2,10 +2,12 @@ package net.bourgau.philippe.concurrency.kata;
 
 import java.util.concurrent.Executors;
 
+import static java.lang.Runtime.getRuntime;
+
 public class ChatRoomFactory {
     public static ChatRoom createChatRoom() {
         return new ConcurrentChatRoom(
                 new InProcessChatRoom(),
-                Executors.newCachedThreadPool());
+                Executors.newFixedThreadPool(getRuntime().availableProcessors()));
     }
 }
